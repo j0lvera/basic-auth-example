047c354e5f0c8d3ed6a85515137d14b5a71891ed
Juan Olvera <olverajuanluis@gmail.com>
2014-10-22 00:04:23 -0500

    signup finished

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-import bottle
-from bottle import Bottle, route, run, response, install, request, abort, parse_auth, auth_basic
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+# import bottle
+from bottle import Bottle, route, run, response, install, request, abort, parse_auth, auth_basic, error, hook
 from passlib.hash import pbkdf2_sha256
 from redis import StrictRedis as Redis
 from hashids import Hashids
@@ -57,60 +56,79 @@ def hash_pass(password):
     return pbkdf2_sha256.encrypt(password, rounds=8000, salt_size=16)
 
 def check_pass(email, password):
-    password_hashed = ''.join(users.find({'email': email})['password'])
-    return pbkdf2_sha256.verify(password, password_hashed)
+    print "yo!"
+    if email == 'thinkxl@thinkxl.net':
+        return True
+    # password_hashed = users.find_one({'email': email})['password']
+    # return pbkdf2_sha256.verify(password, password_hashed)
 
 # def check_token(email, token):
 
 # Enable cors decorator
-def enable_cors(fn):
-    def _enable_cors(*args, **kwargs):
-        # set CORS headers
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+# def enable_cors(fn):
+#     def _enable_cors(*args, **kwargs):
+#         # set CORS headers
+#         response.headers['Access-Control-Allow-Origin'] = '*'
+#         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+#         response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+# 
+#         if bottle.request.method != 'OPTIONS':
+#             # actual request; reply with the actual response
+#                 return fn(*args, **kwargs)
+# 
+#     return _enable_cors
 
-        if bottle.request.method != 'OPTIONS':
-            # actual request; reply with the actual response
-                return fn(*args, **kwargs)
-
-    return _enable_cors
-
+@hook('before_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
 @route('/', method=['OPTIONS', 'GET'])
-@enable_cors
 def hello():
     response.headers['Content-type'] = 'application/json'
     return { 'yo': 'sap' } 
 
-@route('/login', method='POST')
-@enable_cors
+@route('/login', method='GET')
 @auth_basic(check_pass)
 def login():
-    return { 'token': 'figure out how to create a token' }
+    print request.auth
+    return {'token': 'success'}
 
 @route('/signup', method=['OPTIONS', 'POST'])
-@enable_cors
+# @enable_cors
 def create():
     email = post_get('email')
     password = post_get('password')
-    if email is None or password is None:
-        abort(401, "Access Denied") # missing arguments
-    if users.find_one({'email': email}) is not None: # look for the username
-        abort(400, "Existing User") #existing user
-    password_hashed = hash_pass(password)
-    users.insert({'_id': users.find().count() + 1, 'email': email, 'password': password_hashed})
-    token = generate_token(email, password_hashed)
-    save_token(email, token)
-    return {'token': token}
+    print (email is None)
+    print (password is None)
+    if email == '' and password == '':
+        abort(401) # Missing arguments
+    elif users.find_one({'email': email}) is not None: # look for the username
+        abort(400) # Existing user
+    else:
+        password_hashed = hash_pass(password)
+        users.insert({'_id': users.find().count() + 1, 'email': email, 'password': password_hashed})
+        token = generate_token(email, password_hashed)
+        save_token(email, token)
 
 @route('/show', method='GET')
-@enable_cors
+# @enable_cors
 def show():
     # result = users.find_one({'email': 'thinkxl@gmail.com'})['password']
     result = users.find()
     # result = users.find_one({'email': 'thinkxl@gmail.com'})['_id']
 
     return dumps(result)
+
+@error(400)
+# @enable_cors
+def mistake400(code):
+    return "Existing User"
+
+@error(401)
+# @enable_cors
+def mistake400(code):
+    return "Missing Arguments"
 
 run(host='0.0.0.0', port='8080', reloader=True)
